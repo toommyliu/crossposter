a607406f48caae90e6b08ff4ce0cf63401848174
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-02-18 20:59:31 -0800

    fix: specify hostname in nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,11 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	images: {
-		remotePatterns: ["i.redd.it"],
+		remotePatterns: [
+			{
+				hostname: "i.redd.it",
+			},
+		],
 	},
 };
 
