1b66df4223ac3454c621b1b6bc7a262a7add1eca
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-03-19 11:53:41 -0700

    feat(components): add colors to topnav links based on path

--- components/top-nav.tsx
+++ components/top-nav.tsx
@@ -5,26 +5,41 @@ import ThemeChanger from "./theme-changer";
 import TumblrAvatar from "./tumblr/avatar";
 import TumblrSignIn from "./tumblr/sign-in";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "~/lib/utils";
 
 export default function TopNav() {
 	const { tumblrUser } = useStore((store) => store);
+
+	const path = usePathname();
 
 	return (
 		<nav className="w-full px-4">
 			<div className="mt-auto flex h-14 items-center justify-between border-b-2">
 				<div className="flex items-center space-x-4">
-					<Link href="/" className="text-2xl font-bold hover:text-blue-400">
+					<Link
+						href="/"
+						className={cn(
+							"text-2xl font-bold hover:text-blue-400",
+							{ "text-blue-500": path === "/" }
+						)}
+					>
 						crossposter
 					</Link>
 					<Link
 						href="/reddit"
-						className="text-lg hover:text-blue-400"
+						className={cn("text-xl font-bold hover:text-blue-400", {
+							"text-red-500": path === "/reddit"
+						})}
 					>
 						reddit
 					</Link>
 					<Link
 						href="/instagram"
-						className="text-lg hover:text-blue-400"
+						className={cn("text-xl font-bold hover:text-blue-400", {
+							"bg-gradient-to-br from-[#833ab4] via-[#fd1d1d] to-[#fcb045] bg-clip-text text-transparent":
+								path === "/instagram"
+						})}
 					>
 						instagram
 					</Link>
