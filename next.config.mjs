33804ecb8d73f66b86d9b4a44f83c46fef66eeef
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-03-19 11:20:38 -0700

    wip: attempt cors fix

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,10 @@ const nextConfig = {
 				source: "/(.*)",
 				headers: [
 					{ key: "Access-Control-Allow-Credentials", value: "true" },
-					{ key: "Access-Control-Allow-Origin", value: "*" },
+					{
+						key: "Access-Control-Allow-Origin",
+						value: "https://elegant-biscotti-a4f022.netlify.app/reddit"
+					},
 					{
 						key: "Access-Control-Allow-Methods",
 						value: "GET,OPTIONS,PATCH,DELETE,POST,PUT"
