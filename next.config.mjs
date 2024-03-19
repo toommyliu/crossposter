5c414fe7ff76d2504f4ad6bba6b712c0d40d4fb9
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-03-19 10:32:42 -0700

    wip: add cors headers

--- next.config.mjs
+++ next.config.mjs
@@ -14,10 +14,23 @@ const nextConfig = {
 	},
 	// required for SharedArrayBuffer (ffmpeg.wasm)
 	async headers() {
+		console.log(process.env);
 		return [
 			{
 				source: "/(.*)",
 				headers: [
+					{ key: "Access-Control-Allow-Credentials", value: "true" },
+					{
+						key: "Access-Control-Allow-Origin",
+						value:
+							process.env.NODE_ENV === "development"
+								? "localhost:3000"
+								: "crossposter-reddit-to-tumblr.vercel.app"
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET"
+					},
 					{
 						key: "Cross-Origin-Embedder-Policy",
 						value: "require-corp"
