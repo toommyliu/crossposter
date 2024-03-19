3878d58e0fd25d7f91b4444f6b089899a5e117e0
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-03-19 10:20:59 -0700

    fix(components): add hover effect for topnav routes

--- components/top-nav.tsx
+++ components/top-nav.tsx
@@ -4,6 +4,7 @@ import { useStore } from "~/lib/providers/StoreProvider";
 import ThemeChanger from "./theme-changer";
 import TumblrAvatar from "./tumblr/avatar";
 import TumblrSignIn from "./tumblr/sign-in";
+import Link from "next/link";
 
 export default function TopNav() {
 	const { tumblrUser } = useStore((store) => store);
@@ -12,13 +13,21 @@ export default function TopNav() {
 		<nav className="w-full px-4">
 			<div className="mt-auto flex h-14 items-center justify-between border-b-2">
 				<div className="flex items-center space-x-4">
-					<span className="text-2xl font-bold">crossposter</span>
-					<a href="/reddit" className="text-lg hover:text-blue-400">
+					<Link href="/" className="text-2xl font-bold hover:text-blue-400">
+						crossposter
+					</Link>
+					<Link
+						href="/reddit"
+						className="text-lg hover:text-blue-400"
+					>
 						reddit
-					</a>
-					<a href="/instagram" className="text-lg hover:text-blue-400">
+					</Link>
+					<Link
+						href="/instagram"
+						className="text-lg hover:text-blue-400"
+					>
 						instagram
-					</a>
+					</Link>
 				</div>
 				<div className="flex items-center space-x-4">
 					<ThemeChanger />
