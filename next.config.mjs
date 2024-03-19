32ec3a785107f449d8967400d952430dbda8e912
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-03-19 11:19:07 -0700

    wip: attempt cors fix

--- next.config.mjs
+++ next.config.mjs
@@ -18,20 +18,6 @@ const nextConfig = {
 			{
 				source: "/(.*)",
 				headers: [
-					{
-						key: "Cross-Origin-Embedder-Policy",
-						value: "require-corp"
-					},
-					{
-						key: "Cross-Origin-Opener-Policy",
-						value: "same-origin"
-					}
-				]
-			},
-			{
-				// matching all API routes
-				source: "/reddit",
-				headers: [
 					{ key: "Access-Control-Allow-Credentials", value: "true" },
 					{ key: "Access-Control-Allow-Origin", value: "*" },
 					{
@@ -41,13 +27,20 @@ const nextConfig = {
 					{
 						key: "Access-Control-Allow-Headers",
 						value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+					},
+					{
+						key: "Cross-Origin-Embedder-Policy",
+						value: "require-corp"
+					},
+					{
+						key: "Cross-Origin-Opener-Policy",
+						value: "same-origin"
 					}
 				]
 			}
 		];
 	},
 	experimental: {
-		//
 		serverActions: {
 			bodySizeLimit: "10mb"
 		}
